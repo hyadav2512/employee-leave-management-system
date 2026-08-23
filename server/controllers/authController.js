15daabadb0d191c88740cc69b6dc571e4b73f9a4
hyadav2512 <himanshiyadav7011@gmail.com>
2026-08-23 23:18:11 +0530

    next commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const { jwtSecret } = require('../config/config');
 const userService = require('../services/userService');
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Dummy user data for demonstration purpose
 const demoUser = {
